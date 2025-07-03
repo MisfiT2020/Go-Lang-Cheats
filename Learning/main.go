9449fa06645b532627ce9d274a5fe84f355bdd2b
Misfit <akoushik88@gmail.com>
2025-07-03 18:45:33 +0530

    for loop

--- Learning/main.go
+++ Learning/main.go
@@ -1,3 +1,5 @@
+// For Loop
+
 package main
 
 import "fmt"
@@ -6,13 +8,24 @@ func main() {
 	conferenceName := "Go Conference"
 	const ConferenceTickets = 50
 	remainingTickets := 50
+	var booking []string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", ConferenceTickets, remainingTickets)
-	fmt.Println("Please enter your Name and Tickets Needed")
 
-	var userName string //defining type
-	var UserAge uint    // defining the int type. so, user can't book in negative
-	fmt.Scan(&userName, &UserAge)
-	fmt.Printf("the user %v booked %v tickets", userName, UserAge)
+	for {
+
+		var userName string //defining type
+		var UserAge uint    // defining the int type. so, user can't book in negative
+
+		fmt.Println("Please enter your Name and Tickets Needed")
+		fmt.Scan(&userName, &UserAge)
+		fmt.Printf("the user %v booked %v tickets\n", userName, UserAge)
+		booking = append(booking, userName)
+		fmt.Printf("total index %v\n", booking)
+		fmt.Printf("total index %v\n", booking[0])
+		fmt.Printf("total index type %T\n", booking)
+		fmt.Printf("total index type %v\n", len(booking))
+	}
+
 }
