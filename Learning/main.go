5c1a327cea41804b92e1ed2fa505e7385704ba56
Misfit <akoushik88@gmail.com>
2025-07-04 13:00:27 +0530

    if/else statements

--- Learning/main.go
+++ Learning/main.go
@@ -1,4 +1,4 @@
-// For Loop
+// If else
 
 package main
 
@@ -7,25 +7,41 @@ import "fmt"
 func main() {
 	conferenceName := "Go Conference"
 	const ConferenceTickets = 50
-	remainingTickets := 50
+	var remainingTickets uint = 50
 	var booking []string
-
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available\n", ConferenceTickets, remainingTickets)
+	var userTicksets uint
 
 	for {
 
 		var userName string //defining type
-		var UserAge uint    // defining the int type. so, user can't book in negative
+		//var UserAge uint    // defining the int type. so, user can't book in negative
+
+		fmt.Printf("Welcome to %v booking application\n", conferenceName)
 
 		fmt.Println("Please enter your Name and Tickets Needed")
-		fmt.Scan(&userName, &UserAge)
-		fmt.Printf("the user %v booked %v tickets\n", userName, UserAge)
-		booking = append(booking, userName)
-		fmt.Printf("total index %v\n", booking)
-		fmt.Printf("total index %v\n", booking[0])
-		fmt.Printf("total index type %T\n", booking)
-		fmt.Printf("total index type %v\n", len(booking))
+		fmt.Scan(&userName, &userTicksets)
+		fmt.Printf("the user %v booked %v tickets\n", userName, userTicksets)
+
+		if userTicksets <= remainingTickets {
+			remainingTickets := remainingTickets - userTicksets
+			fmt.Printf("these tickets are available %v", remainingTickets)
+			fmt.Printf("We have total of %v tickets and %v are still available\n", ConferenceTickets, remainingTickets)
+
+			booking = append(booking, userName)
+			fmt.Printf("total index %v\n", booking)
+			fmt.Printf("total index %v\n", booking[0])
+			fmt.Printf("total index type %T\n", booking)
+			fmt.Printf("total index type %v\n", len(booking))
+
+			if remainingTickets == 0 {
+				fmt.Println("Our Conference Tickets has ended...")
+				break
+			}
+
+		} else {
+			fmt.Printf("you can't choose more than available tickets.... available tickets: %v", remainingTickets)
+		}
+
 	}
 
 }
